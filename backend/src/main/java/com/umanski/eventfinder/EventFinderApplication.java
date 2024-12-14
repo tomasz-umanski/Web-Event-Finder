366b9155d6a353053322c9ae7eecc220eba01bfb
tomasz-umanski <tomasz.umanski@gmail.com>
2024-12-14 19:05:29 +0100

    feat: initialize backend configuration

---
+++ backend/src/main/java/com/umanski/eventfinder/EventFinderApplication.java
@@ -0,0 +1,13 @@
+package com.umanski.eventfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFinderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/umanski/eventfinder/EventFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umanski.eventfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
